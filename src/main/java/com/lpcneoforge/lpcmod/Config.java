6c3679e2bae6f93d5d99495f226feca1bdeee2c2
Vitalii <fangnes2004@gmail.com>
2025-08-29 20:32:00 -0400

    removed busSubscriber from Config

--- src/main/java/com/lpcneoforge/lpcmod/Config.java
+++ src/main/java/com/lpcneoforge/lpcmod/Config.java
@@ -1,11 +1,7 @@
 package com.lpcneoforge.lpcmod;
 
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-@EventBusSubscriber(modid = LPCNeoForge.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class Config {
   private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
   public static final ModConfigSpec SPEC;
